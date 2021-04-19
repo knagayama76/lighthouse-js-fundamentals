5b7e1bc9c74432dcb80a037405eb4a415895a9fc
Kaoru Nagayama <kaoair@hotmail.com>
2021-04-19 10:28:48 -0400

    learning isEven function

---
+++ even.js
@@ -0,0 +1,13 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+};
+
+// const tenIsEven = isEven(10);
+// const elevenIsEven = isEven(11);
+
+// console.log(tenIsEven); // true
+// console.log(elevenIsEven); // false
+
+// calling the function and print out at the same time
+console.log(isEven(10)); // true
+console.log(isEven(11)); // true
